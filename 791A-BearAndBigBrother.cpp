0d1ff97c94d68e6dc2a1142ab9df1102b886bf83
Aayush <aayushjindal65@gmail.com>
2026-06-03 23:30:19 +0530

    Bear And Big-Brother

---
+++ 791A-BearAndBigBrother.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    int years = 0;
+    while (a <= b) {
+        a *= 3;  // Limak's weight triples
+        b *= 2;  // Bob's weight doubles
+        years++;
+    }
+
+    cout << years << "\n";
+
+    return 0;
+}
